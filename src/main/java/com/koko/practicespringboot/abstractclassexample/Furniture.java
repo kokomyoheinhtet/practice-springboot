c4377d86d3a6314a49b3e23efdd751b06e5555b6
kokomyoheinhtet <kokomyoheinhtet@gmail.com>
2023-11-27 11:05:39 +0700

    added Abstract Class Example

---
+++ src/main/java/com/koko/practicespringboot/abstractclassexample/Furniture.java
@@ -0,0 +1,16 @@
+package com.koko.practicespringboot.abstractclassexample;
+
+public class Furniture extends ProductForSale {
+
+    public Furniture(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+        System.out.println("This " + type + " was manufacture in North Carolina.");
+        System.out.printf("The price of the piece is $%6.2f %n", price);
+        System.out.println(description);
+    }
+}
---
+++ src/main/java/com/koko/practicespringboot/abstractclassexample/Store.java
@@ -0,0 +1,57 @@
+package com.koko.practicespringboot.abstractclassexample;
+
+import java.util.ArrayList;
+
+record OrderItem(int qty, ProductForSale product) {
+}
+
+public class Store {
+
+    private static ArrayList<ProductForSale> storeProducts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        storeProducts.add(new ArtObject("Oil Painting", 1350,
+                "work by ABF"));
+        storeProducts.add(new ArtObject("Sculpture", 2000,
+                "by JKF"));
+        storeProducts.add(new Furniture("Desk", 500,
+                "Mahogany Desk"));
+        storeProducts.add(new Furniture("Lamp", 200,
+                "Tiffany Lamp"));
+        listProducts();
+
+        System.out.println("\nOrder 1");
+        ArrayList<OrderItem> order1 = new ArrayList<>();
+        addItemToOrder(order1, 1, 2);
+        addItemToOrder(order1, 0, 1);
+        printOrder(order1);
+
+        System.out.println("\nOrder 2");
+        ArrayList<OrderItem> order2 = new ArrayList<>();
+        addItemToOrder(order2, 3, 5);
+        addItemToOrder(order2, 0, 1);
+        addItemToOrder(order2, 2, 1);
+        printOrder(order2);
+    }
+
+    public static void listProducts() {
+        for (ProductForSale item : storeProducts) {
+            System.out.println("-".repeat(30));
+            item.showDetails();
+        }
+    }
+
+    public static void addItemToOrder(ArrayList<OrderItem> order, int orderIndex,
+                                      int qty) {
+        order.add(new OrderItem(qty, storeProducts.get(orderIndex)));
+    }
+
+    public static void printOrder(ArrayList<OrderItem> order) {
+        double salesTotal = 0;
+        for (OrderItem item : order) {
+            item.product().printPricedItem(item.qty());
+            salesTotal += item.product().getSalesPrice((item.qty()));
+        }
+        System.out.printf("Sales Total = $%6.2f %n", salesTotal);
+    }
+}
